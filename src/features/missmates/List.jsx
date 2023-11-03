0f2e9acbecfb758bc21cf63f22dc413b8512e376
Alejandro Muñoz <alex12munozgeo@hotmail.es>
2023-11-03 15:09:27 +0100

    added

--- src/features/missmates/List.jsx
+++ src/features/missmates/List.jsx
@@ -1,16 +1,17 @@
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-// import { loadMissmates } from "./missmatesSlice";
+import { loadMissmates } from "./missmatesSlice";
 
 function List() {
   const [listOfMissmates, setListOfMissmates] = useState([]);
-  // const dispatch = useDispatch();
+  const dispatch = useDispatch();
   const { missmates } = useSelector((store) => store.missmates);
 
   useEffect(() => {
+    dispatch(loadMissmates());
     setListOfMissmates(missmates);
-    console.log(missmates);
+    console.log(listOfMissmates);
   }, []);
 
   return (
